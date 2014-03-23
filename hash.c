af8c7f47ae485d92314b25e0c412c1e1ddba7d6c
Edward Soo <paktiksoo@gmail.com>
2014-03-23 01:51:01 -0700

    hash delete

--- hash.c
+++ hash.c
@@ -29,7 +29,12 @@ hash_t* hash_new (int size) {
 }
 
 void hash_destroy (hash_t **hash_p) {
+  int i;
   hash_t *hash = *hash_p;
+  for (i = 0; i < hash->size; i++) {
+    if (!hash->array[i]) continue;
+    list_destroy (hash->array + i);
+  }
   free (hash->array);
   free (hash);
   *hash_p = NULL;
@@ -59,6 +64,9 @@ void list_destroy (list_t **list_p) {
   list_t * list;
   list = *list_p;
   free (list->key);
+  if (list->next) {
+    list_destroy (&(list->next));
+  }
   free (list);
   *list_p = NULL;
 }
@@ -67,16 +75,15 @@ void hash_insert (hash_t *hash, uint8_t *key, int key_len, uint64_t value) {
   int h, diff;
   list_t **list_p, *list;
 
-    h = hash_code (key, key_len) % hash->size;
-
+  h = hash_code (key, key_len) % hash->size;
   list_p = hash->array + h;
 
   // Keep entries sorted by key_len then key values in collision list
   while (*list_p) {
-        list = *list_p;
+    list = *list_p;
 
     if (list->key_len > key_len) {
-            *list_p = list_new (key, key_len, value, list);
+      *list_p = list_new (key, key_len, value, list);
       hash->count += 1;
       return;
 
@@ -84,25 +91,59 @@ void hash_insert (hash_t *hash, uint8_t *key, int key_len, uint64_t value) {
       diff = memcmp (key, list->key, key_len);
 
       if (diff == 0) {
-                // Keys are the same, update value
+        // Keys are the same, update value
         list->value = value;
         hash->count += 1;
         return;
 
       } else if (diff < 0) {
-                // New key value is less than this node's key
+        // New key value is less than this node's key
         *list_p = list_new (key, key_len, value, list);
         hash->count += 1;
         return;
       }
-          }
+    }
     list_p = &(list->next);
   }
-    *list_p = list_new (key, key_len, value, NULL);
+  *list_p = list_new (key, key_len, value, NULL);
   hash->count += 1;
 }
 
 void hash_delete (hash_t *hash, uint8_t *key, int key_len,
-    int (*fn)(uint64_t value, uint64_t), uint64_t arg);
+    int (*fn)(uint64_t value, uint64_t arg), uint64_t arg) {
+  int h, diff;
+  list_t **list_p, *list;
+
+  h = hash_code (key, key_len) % hash->size;
+  list_p = hash->array + h;
+
+  while (*list_p) {
+    list = *list_p;
+
+    if (list->key_len > key_len) {
+      // Key not in table
+      return;
+
+    } else if (list->key_len == key_len) {
+      diff = memcmp (key, list->key, key_len);
+
+      if (diff == 0) {
+        // Found key
+        if (!fn || fn (list->value, arg)) {
+          *list_p = list->next;
+          list->next = NULL;
+          list_destroy (&list);
+          hash->count -= 1;
+          return;
+        }
+
+      } else if (diff < 0) {
+        // Key not in table
+        return;
+      }
+    }
+    list_p = &(list->next);
+  }
+}
 
 int hash_lookup (hash_t *hash, uint8_t *key, int key_len, uint64_t *value);
--- hash.h
+++ hash.h
@@ -18,7 +18,9 @@ hash_t* hash_new (int size);
 void hash_destroy (hash_t **hash_p);
 void hash_insert (hash_t *hash, uint8_t *key, int key_len, uint64_t value);
 void hash_delete (hash_t *hash, uint8_t *key, int key_len,
-    int (*fn)(uint64_t value, uint64_t), uint64_t arg);
+    int (*fn)(uint64_t value, uint64_t arg), uint64_t arg);
 int hash_lookup (hash_t *hash, uint8_t *key, int key_len, uint64_t *value);
+list_t* list_new (uint8_t *key, int key_len, uint64_t value, list_t *next);
+void list_destroy (list_t **list_p);
 
 #endif
